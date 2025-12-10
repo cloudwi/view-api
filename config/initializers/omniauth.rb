90b05af01835c2138c241357ef8a2c9eaac46f03
cloudwi <cloudwiiiii@gmail.com>
2025-12-10 15:15:57 +0900

    Add profile_nickname and profile_image to kakao oauth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :kakao,
            Rails.application.credentials.dig(:kakao, :client_id),
-           Rails.application.credentials.dig(:kakao, :client_secret)
+           Rails.application.credentials.dig(:kakao, :client_secret),
+           scope: "account_email,profile_nickname,profile_image"
 end
 
 OmniAuth.config.allowed_request_methods = [ :post, :get ]
