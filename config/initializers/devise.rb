183a364d655a52352a79e577992f13c28e470c57
cloudwi <cloudwiiiii@gmail.com>
2025-12-10 15:03:42 +0900

    Simplify devise.rb to minimum required config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,13 +2,4 @@
 
 Devise.setup do |config|
   require "devise/orm/active_record"
-
-  config.mailer_sender = "noreply@example.com"
-  config.case_insensitive_keys = [ :email ]
-  config.strip_whitespace_keys = [ :email ]
-  config.skip_session_storage = [ :http_auth ]
-  config.stretches = Rails.env.test? ? 1 : 12
-  config.sign_out_via = :delete
-  config.responder.error_status = :unprocessable_entity
-  config.responder.redirect_status = :see_other
 end
