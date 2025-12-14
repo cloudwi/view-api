a7082990f7a17d5627bef1be33795697d9de6046
cloudwi <cloudwi@gmail.com>
2025-12-14 16:52:07 +0900

    chore: Ruby 버전 및 의존성 업데이트, 스키마 정리
    - Ruby 버전 3.4.6 반영
    - Gemfile.lock에 arm64-darwin-24 플랫폼 추가
    - User 모델 스키마 정리 (불필요한 컬럼 제거)

--- db/schema.rb
+++ db/schema.rb
@@ -24,20 +24,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_062039) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name"
     t.string "nickname"
-    t.string "profile_image"
     t.string "provider"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
     t.string "uid"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "view_options", force: :cascade do |t|
